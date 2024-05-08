9b06bc345ded70d9fad7526b1e9f3863748a8b36
danielkembeu <kembeudaniel2@gmail.com>
2024-05-08 17:29:20 +0100

    Ajout des images et du CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import image1 from './assets/images/image1.jpg';
+import './App.css';
+
 
 export default function App() {
     return (
-        <div>
+        <div className='container'>
             <h2>Hello world</h2>
+            <img
+                alt='Container image'
+                src={image1}
+                className='image' />
         </div>
     )
 }
